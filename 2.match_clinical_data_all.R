05ca25cf9b58d1742df987f02430b39c497d12d8
lilihub <lili.zhang27@mail.dcu.ie>
2023-04-19 10:33:38 +0000

    regenerate choice data matched with clinical data and exclude incomplete cases

--- 2.match_clinical_data_all.R
+++ 2.match_clinical_data_all.R
@@ -1,175 +1,111 @@
 ## import clinical data
 clinical_data = read.csv("all_data/srData_cc.csv")
+## import IDs that need to be removed
+incomplete_cases = read.csv("all_data/incomplete_cases.csv")
 
-############################# CBT_baseline ####################
+############################# CBT_baseline 682 participants####################
 clinical_temp = subset(clinical_data, clinical_data$treatment_group == 'cbt' &
-                         !is.na(clinical_data$AD))
+                         !is.na(clinical_data$AD) & !is.na(clinical_data$AD_fu) &
+                         !(clinical_data$subjID %in% incomplete_cases$ncPpts))
 CBT_baseline = read.csv("clean_data/CBT_baseline.csv")
 
 # format ID_short in clinical data
-clinical_temp['subjID'] <- sub("C", "C1", clinical_temp$ID_short)
+CBT_baseline['subjID'] <- sub("C1", "C", CBT_baseline$subjID)
 intersectIDs = intersect(clinical_temp$subjID,unique(CBT_baseline$subjID))
 # subset CBT_baseline (subjects with clinical match)
-CBT_baseline_matched = subset(CBT_baseline, 
+CBT_baseline_matched = subset(CBT_baseline,
                               CBT_baseline$subjID %in% intersectIDs)
-write.csv(CBT_baseline_matched, 
+write.csv(CBT_baseline_matched,
           file = 'clean_data/CBT_baseline_matched.csv',
           row.names = FALSE)
 clinical_CBT_baseline = subset(clinical_temp, clinical_temp$subjID %in% intersectIDs)
-write.csv(clinical_CBT_baseline, 
+write.csv(clinical_CBT_baseline,
           file = 'clean_data/clinical_CBT_baseline.csv',
           row.names = FALSE)
 # clinical_CBT_baseline_scale <- clinical_CBT_baseline %>%
 #   dplyr::mutate_at(c('AES_total', 'AUDIT_total', 'BIS_total', 'EAT_total',
-#                      'LSAS_total', 'OCI_total', 'SCZ_total','SDS_total', 
+#                      'LSAS_total', 'OCI_total', 'SCZ_total','SDS_total',
 #                      'STAI_total', 'QIDS_total', 'WSAS_total'), ~(scale(.) %>% as.vector))
-# write.csv(clinical_CBT_baseline_scale, 
+# write.csv(clinical_CBT_baseline_scale,
 #           file = 'clean_data/clinical_CBT_baseline_scale.csv',
 #           row.names = FALSE)
 
 
-############################# CBT_followup ####################
+############################# CBT_followup 682 participants####################
 CBT_followup = read.csv("clean_data/CBT_followup.csv")
 
 # format ID_short in clinical data
-clinical_temp['sessionID'] <- sub("C1", "C4", clinical_temp$sessionID)
-intersectIDs = intersect(clinical_temp$sessionID,unique(CBT_followup$subjID))
+CBT_followup['subjID'] <- sub("C4", "C", CBT_followup$subjID)
+intersectIDs = intersect(clinical_temp$subjID,unique(CBT_followup$subjID))
 # subset CBT_followup (subjects with clinical match)
-CBT_followup_matched = subset(CBT_followup, 
+CBT_followup_matched = subset(CBT_followup,
                               CBT_followup$subjID %in% intersectIDs)
-write.csv(CBT_followup_matched, 
+write.csv(CBT_followup_matched,
           file = 'clean_data/CBT_followup_matched.csv',
           row.names = FALSE)
-clinical_CBT_followup = subset(clinical_temp, clinical_temp$sessionID %in% intersectIDs)
-write.csv(clinical_CBT_followup, 
+clinical_CBT_followup = subset(clinical_temp, clinical_temp$subjID %in% intersectIDs)
+write.csv(clinical_CBT_followup,
           file = 'clean_data/clinical_CBT_followup.csv',
           row.names = FALSE)
 # clinical_CBT_followup_scale <- clinical_CBT_followup %>%
 #   dplyr::mutate_at(c('AES_total', 'AUDIT_total', 'BIS_total', 'EAT_total',
-#                      'LSAS_total', 'OCI_total', 'SCZ_total','SDS_total', 
+#                      'LSAS_total', 'OCI_total', 'SCZ_total','SDS_total',
 #                      'STAI_total', 'QIDS_total', 'WSAS_total'), ~(scale(.) %>% as.vector))
-# write.csv(clinical_CBT_followup_scale, 
+# write.csv(clinical_CBT_followup_scale,
 #           file = 'clean_data/clinical_CBT_followup_scale.csv',
 #           row.names = FALSE)
 
 
-############################# SSRI_baseline ####################
-clinical_temp = subset(clinical_data, clinical_data$treatment == 'ssri' 
-                       & clinical_data$session == 'baseline')
-CBT_baseline = read.csv("clean_data/CBT_baseline.csv")
+############################# SSRI_baseline 95 participants####################
+clinical_temp = subset(clinical_data, clinical_data$treatment == 'ssri' &
+                       !is.na(clinical_data$AD) & !is.na(clinical_data$AD_fu) &
+                       !(clinical_data$subjID %in% incomplete_cases$ncPpts))
+SSRI_baseline = read.csv("clean_data/SSRI_baseline.csv")
 
 # format ID_short in clinical data
-clinical_temp['subjID'] <- sub("C", "C1", clinical_temp$ID_short)
-intersectIDs = intersect(clinical_temp$subjID,unique(CBT_baseline$subjID))
+SSRI_baseline['subjID'] <- sub(".", "S", SSRI_baseline$subjID)
+intersectIDs = intersect(clinical_temp$subjID, unique(SSRI_baseline$subjID))
 # subset CBT_baseline (subjects with clinical match)
-CBT_baseline_matched = subset(CBT_baseline, 
-                              CBT_baseline$subjID %in% intersectIDs)
-write.csv(CBT_baseline_matched, 
-          file = 'clean_data/CBT_baseline_matched.csv',
+SSRI_baseline_matched = subset(SSRI_baseline,
+                               SSRI_baseline$subjID %in% intersectIDs)
+write.csv(SSRI_baseline_matched,
+          file = 'clean_data/SSRI_baseline_matched.csv',
           row.names = FALSE)
-clinical_CBT_baseline = subset(clinical_temp, clinical_temp$subjID %in% intersectIDs)
-write.csv(clinical_CBT_baseline, 
-          file = 'clean_data/clinical_CBT_baseline.csv',
-          row.names = FALSE)
-clinical_CBT_baseline_scale <- clinical_CBT_baseline %>%
-  dplyr::mutate_at(c('AES_total', 'AUDIT_total', 'BIS_total', 'EAT_total',
-                     'LSAS_total', 'OCI_total', 'SCZ_total','SDS_total', 
-                     'STAI_total', 'QIDS_total', 'WSAS_total'), ~(scale(.) %>% as.vector))
-write.csv(clinical_CBT_baseline_scale, 
-          file = 'clean_data/clinical_CBT_baseline_scale.csv',
+clinical_SSRI_baseline = subset(clinical_temp, clinical_temp$subjID %in% intersectIDs)
+write.csv(clinical_SSRI_baseline,
+          file = 'clean_data/clinical_SSRI_baseline.csv',
           row.names = FALSE)
 
 
+############################# SSRI_followup 95 participants####################
+SSRI_followup = read.csv("clean_data/SSRI_followup.csv")
 
-
-
-
-
-
-
-
-
-
-
-
-
-# SSRI_baseline_clinical = subset(clinical_data, clinical_data$treatment == 'ssri' 
-#                                & clinical_data$session == 'baseline')
-
-# SSRI_baseline_clinical$ID_short <- paste0("1", SSRI_baseline_clinical$ID_short)
-# subset CBT_baseline1_stable and CBT_baseline1_volatile (subjects with clinical match)
-CBT_baseline1_stable_match_clinical = subset(CBT_baseline1_stable, 
-                                             CBT_baseline1_stable$subjID 
-                                             %in% CBT_baseline_clinical$ID_short)
-CBT_baseline1_volatile_match_clinical = subset(CBT_baseline1_volatile,
-                                               CBT_baseline1_volatile$subjID 
-                                               %in% CBT_baseline_clinical$ID_short)
-
-write.csv(CBT_baseline1_stable_match_clinical, file = 'clean_data/CBT_baseline1_stable_match_clinical.csv',
+# format ID_short in clinical data
+SSRI_followup['subjID'] <- sub(".", "S", SSRI_followup$subjID)
+intersectIDs = intersect(clinical_temp$subjID,unique(SSRI_followup$subjID))
+# subset CBT_baseline (subjects with clinical match)
+SSRI_followup_matched = subset(SSRI_followup,
+                               SSRI_followup$subjID %in% intersectIDs)
+write.csv(SSRI_followup_matched,
+          file = 'clean_data/SSRI_followup_matched.csv',
           row.names = FALSE)
-write.csv(CBT_baseline1_volatile_match_clinical, file = 'clean_data/CBT_baseline1_volatile_match_clinical.csv',
-          row.names = FALSE)
-
-# subset CBT_baseline2_stable and CBT_baseline2_volatile (subjects with clinical match)
-CBT_baseline2_stable_match_clinical = subset(CBT_baseline2_stable, 
-                                             CBT_baseline2_stable$subjID 
-                                             %in% CBT_baseline_clinical$ID_short)
-CBT_baseline2_volatile_match_clinical = subset(CBT_baseline2_volatile,
-                                               CBT_baseline2_volatile$subjID 
-                                               %in% CBT_baseline_clinical$ID_short)
-
-write.csv(CBT_baseline2_stable_match_clinical, file = 'clean_data/CBT_baseline2_stable_match_clinical.csv',
-          row.names = FALSE)
-write.csv(CBT_baseline2_volatile_match_clinical, file = 'clean_data/CBT_baseline2_volatile_match_clinical.csv',
+clinical_SSRI_followup = subset(clinical_temp, clinical_temp$subjID %in% intersectIDs)
+write.csv(clinical_SSRI_followup,
+          file = 'clean_data/clinical_SSRI_followup.csv',
           row.names = FALSE)
 
 
-SSRI_baseline1_stable_match_clinical = subset(SSRI_baseline1_stable, 
-                                              SSRI_baseline1_stable$subjID 
-                                              %in% SSRI_baseline_clinical$ID_short)
-SSRI_baseline1_volatile_match_clinical = subset(SSRI_baseline1_volatile,
-                                                SSRI_baseline1_volatile$subjID 
-                                                %in% SSRI_baseline_clinical$ID_short)
-
-write.csv(SSRI_baseline1_stable_match_clinical, file = 'clean_data/SSRI_baseline1_stable_match_clinical.csv',
-          row.names = FALSE)
-write.csv(SSRI_baseline1_volatile_match_clinical, file = 'clean_data/SSRI_baseline1_volatile_match_clinical.csv',
-          row.names = FALSE)
-SSRI_baseline2_stable_match_clinical = subset(SSRI_baseline2_stable, 
-                                              SSRI_baseline2_stable$subjID 
-                                              %in% SSRI_baseline_clinical$ID_short)
-SSRI_baseline2_volatile_match_clinical = subset(SSRI_baseline2_volatile,
-                                                SSRI_baseline2_volatile$subjID 
-                                                %in% SSRI_baseline_clinical$ID_short)
-
-write.csv(SSRI_baseline2_stable_match_clinical, file = 'clean_data/SSRI_baseline2_stable_match_clinical.csv',
-          row.names = FALSE)
-write.csv(SSRI_baseline2_volatile_match_clinical, file = 'clean_data/SSRI_baseline2_volatile_match_clinical.csv',
-          row.names = FALSE)
 
 
-######################################################################################
-# subset CBT_baseline_clinical (CBT_baseline subjects who did schedule1)  
-CBT_baseline1_clinical = subset(CBT_baseline_clinical, CBT_baseline_clinical$ID_short
-                                %in% unique(CBT_baseline1_stable_match_clinical$subjID))
-write.csv(CBT_baseline1_clinical, file = 'clean_data/CBT_baseline1_clinical.csv', row.names = FALSE)
-# subset CBT_baseline_clinical (CBT_baseline subjects who did schedule2)  
-CBT_baseline2_clinical = subset(CBT_baseline_clinical, CBT_baseline_clinical$ID_short
-                                %in% unique(CBT_baseline2_match_clinical$subjID))
-write.csv(CBT_baseline2_clinical, file = 'clean_data/CBT_baseline2_clinical.csv', row.names = FALSE)
 
 
-# subset CBT_baseline1 and CBT_baseline2 (subjects with clinical match)
-CBT_baseline1_match_clinical = subset(CBT_baseline1, 
-                                      CBT_baseline1$subjID 
-                                      %in% CBT_baseline_clinical$ID_short)
-CBT_baseline2_match_clinical = subset(CBT_baseline2,
-                                      CBT_baseline2$subjID 
-                                      %in% CBT_baseline_clinical$ID_short)
 
-write.csv(CBT_baseline1_match_clinical, file = 'clean_data/CBT_baseline1_match_clinical.csv',
-          row.names = FALSE)
-write.csv(CBT_baseline2_match_clinical, file = 'clean_data/CBT_baseline2_match_clinical.csv',
-          row.names = FALSE)
+
+
+
+
+
+
+
 
 
